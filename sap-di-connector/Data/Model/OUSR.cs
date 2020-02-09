efc3575756d6ec5e71dc2f5263ae674cb3ab4d51
Daniel Maximo <d.maximo@silifalcon.com>
2020-02-09 01:12:59 -0600

    Agrega modelo de usuarios

---
+++ sap-di-connector/Data/Model/OUSR.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDevman.SAPConnector.Data.Model
+{
+
+	/// <summary>
+	/// User model
+	/// </summary>
+	public class OUSR
+	{
+
+		/// <summary>
+		/// Gets or sets user id
+		/// </summary>
+		public int USERID { get; set; }
+
+		/// <summary>
+		/// Gets or sets user code
+		/// </summary>
+		public string USER_CODE { get; set; }
+
+		/// <summary>
+		/// Gets or sets user name
+		/// </summary>
+		public string U_NAME { get; set; }
+
+		/// <summary>
+		/// Gets or sets email
+		/// </summary>
+		public string E_Mail { get; set; }
+
+		/// <summary>
+		/// Gets or sets superuser
+		/// Y - yes
+		/// N - No
+		/// </summary>
+		public string SUPERUSER { get; set; }
+
+	}
+}
